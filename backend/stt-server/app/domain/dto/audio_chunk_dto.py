f94e80806e150d4b06dd2263ea1c834ff6629dc9
kimzes <117077471+wonder002@users.noreply.github.com>
2024-11-06 10:13:15 +0900

    feat: dto 구현

--- backend/stt-server/app/domain/dto/audio_chunk_dto.py
+++ backend/stt-server/app/domain/dto/audio_chunk_dto.py
@@ -1,5 +1,10 @@
 from pydantic import BaseModel
+import io
+
 
 class AudioChunk(BaseModel):
     chunk_id: int
-    chunk_data: bytes
+    chunk_data: io.BytesIO
+
+    class Config:
+        arbitrary_types_allowed = True  # BytesIO와 같은 임의 타입을 허용
