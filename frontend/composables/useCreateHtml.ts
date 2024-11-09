451b7e0d69368e7c7911b400db1464d932e9eff6
Ywoosang <opellong13@gmail.com>
2024-11-10 05:03:11 +0900

    feat: 해설 html 생성용 컴포저블 추가

--- frontend/composables/useCreateHtml.ts
+++ frontend/composables/useCreateHtml.ts
@@ -1,45 +1,24 @@
-import type {Commentary} from "~/types/CommentaryStore";
 import {marked, Renderer} from "marked";
 import hljs from "highlight.js";
 import DOMPurify from "dompurify";
 import "highlight.js/styles/github-dark.css";
 
 export const useCreateHtml = () => {
-    const createHtmlFromCommentary = async (commentary: Commentary): Promise<string> => {
+    const createHtmlFromCommentary = async (startTime: number, content: string): Promise<string> => {
         const renderer = new Renderer();
-        renderer.code = ({ text, lang }: { text: string; lang?: string }): string => {
-            const language = lang && hljs.getLanguage(lang) ? lang : "plaintext";
-            const highlightedCode = hljs.highlight(text, { language }).value;
-            return `<pre><code class="hljs ${language}">${highlightedCode}</code></pre>`;
-        };
-
-        const rawHtml = await marked(commentary.content, { renderer })
+        renderer.code = highlightCode;
+        const rawHtml = await marked(content, { renderer })
         const sanitizedHtml = DOMPurify.sanitize(rawHtml);
-        console.log(commentary.startTime);
-        const videoTime = formatTime(commentary.startTime);
-        return `<div id="content-${commentary.startTime}" class="content-section q-shadow-xs"><div class="time-Badge-root"><span class="time-Badge-inner">${videoTime}</span></div>${sanitizedHtml}</div>`;
+        return `<div class="c-CT-inner">${sanitizedHtml}</div>`;
     }
 
-    const createHtmlFromCommentaries = async (commentaries: Commentary[]): Promise<string> => {
-        const htmlArray = await Promise.all(commentaries.map(createHtmlFromCommentary));
-        return htmlArray.join("");
-    }
-
-    const formatTime = (floatSeconds: number): string => {
-        const totalSeconds = Math.floor(floatSeconds); // 정수 초로 변환
-        const hours = Math.floor(totalSeconds / 3600);
-        const minutes = Math.floor((totalSeconds % 3600) / 60);
-        const seconds = totalSeconds % 60;
-
-        if (hours > 0) {
-            return `${hours}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-        } else {
-            return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-        }
+    const highlightCode = ({ text, lang } : { text: string, lang?: string }): string => {
+        const language = lang && hljs.getLanguage(lang) ? lang : "plaintext";
+        const highlightedCode = hljs.highlight(text, { language }).value;
+        return `<pre><code class="hljs ${language}">${highlightedCode}</code></pre>`;
     }
 
     return {
-        createHtmlFromCommentaries,
         createHtmlFromCommentary
     }
 }
