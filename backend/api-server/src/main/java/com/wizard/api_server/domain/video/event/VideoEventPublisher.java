36e25d4cb3fb482439094d1ff51e5af8e3bef5b7
Ywoosang <opellong13@gmail.com>
2024-11-10 17:13:13 +0900

    refactor: VideoEventPublisher로 주입받도록 리팩토링

---
+++ backend/api-server/src/main/java/com/wizard/api_server/domain/video/event/VideoEventPublisher.java
@@ -0,0 +1,6 @@
+package com.wizard.api_server.domain.video.event;
+
+import com.wizard.api_server.domain.event.DomainEventPublisher;
+
+public interface VideoEventPublisher extends DomainEventPublisher {
+}
