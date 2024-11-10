b9c6e1bb1798eaf3420389521badfd2aa079c084
Ywoosang <opellong13@gmail.com>
2024-11-10 16:58:24 +0900

    [BE/API] video 도메인의 Kafka Consumer를 구현한다. (#10)
    * feat: VideoCommentary DTO 추가
    
    * feat: Json 역직렬화 설정
    
    * feat: Consumer 구현

--- backend/api-server/src/main/java/com/wizard/api_server/external/kafka/config/KafkaConsumerConfig.java
+++ backend/api-server/src/main/java/com/wizard/api_server/external/kafka/config/KafkaConsumerConfig.java
@@ -1,5 +1,6 @@
 package com.wizard.api_server.external.kafka.config;
 
+import com.wizard.api_server.external.kafka.video.dto.VideoCommentary;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -9,28 +10,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 @Configuration
 public class KafkaConsumerConfig {
 
     @Bean
-    public ConsumerFactory<String, Object> consumerFactory() {
+    public ConsumerFactory<String, VideoCommentary> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_1");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
 
-        return new DefaultKafkaConsumerFactory<>(config);
+        JsonDeserializer<VideoCommentary> jsonDeserializer = new JsonDeserializer<>(VideoCommentary.class, false);
+        ErrorHandlingDeserializer<VideoCommentary> errorHandlingDeserializer = new ErrorHandlingDeserializer<>(jsonDeserializer);
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), errorHandlingDeserializer);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    public ConcurrentKafkaListenerContainerFactory<String, VideoCommentary> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, VideoCommentary> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
-
         return factory;
     }
-
-
 }
