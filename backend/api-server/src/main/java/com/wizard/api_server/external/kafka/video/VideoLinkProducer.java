3b98050fc08b6b459a70c2981eebb63f5cd0a4b5
Ywoosang <opellong13@gmail.com>
2024-11-10 17:11:21 +0900

    chore: topic 이름 변경

--- backend/api-server/src/main/java/com/wizard/api_server/external/kafka/video/VideoLinkProducer.java
+++ backend/api-server/src/main/java/com/wizard/api_server/external/kafka/video/VideoLinkProducer.java
@@ -1,19 +1,19 @@
-package com.wizard.api_server.external.kafka.producer;
+package com.wizard.api_server.external.kafka.video;
 
 import com.wizard.api_server.domain.event.DomainEvent;
-import com.wizard.api_server.domain.event.DomainEventPublisher;
+import com.wizard.api_server.domain.video.event.VideoEventPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class VideoLinkProducer implements DomainEventPublisher {
+public class VideoLinkProducer implements VideoEventPublisher {
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     @Override
     public void publish(DomainEvent event) {
-        String topic = "video-event";
+        String topic = "video-link-events";
         kafkaTemplate.send(topic, event);
     }
 }
