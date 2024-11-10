2fd6660d7831d2b719f899618f08e1e223fc9e55
Ywoosang <opellong13@gmail.com>
2024-11-11 03:30:17 +0900

    feat: SSE 이벤트 발행 메소드 추가

--- backend/api-server/src/main/java/com/wizard/api_server/web/sse/SseEmitters.java
+++ backend/api-server/src/main/java/com/wizard/api_server/web/sse/SseEmitters.java
@@ -14,7 +14,7 @@ public class SseEmitters {
 
     public void addEmiter(String connectId, SseEmitter emitter) {
         emitters.put(connectId, emitter);
-        log.info("Added emitter {}", connectId);;
+        log.info("Added emitter {}", connectId);
         log.info("emitter list size: {}", emitters.size());
 
         emitter.onCompletion(() -> {
@@ -29,8 +29,28 @@ public class SseEmitters {
     }
 
     public void sendConnectEvent(SseEmitter emitter) throws IOException {
-            var event = SseEmitter.event()
-                    .name("connect");
+        var event = SseEmitter.event()
+                    .name("connect")
+                            .data("send connect event");
             emitter.send(event);
+        log.info("emitter sent first event");
     }
+
+    public void sendEvent(String requestId, String data) throws IOException {
+        SseEmitter emitter = emitters.get(requestId);
+        if (emitter != null) {
+            var event = SseEmitter.event()
+                    .name("commentary")
+                    .data(data);
+
+            emitter.send(event);
+            log.info("emitter sent comment event");
+        } else {
+            log.warn("emitter not found");
+        }
+    }
+
+    /**
+     *   {"requestId": "3ddfd9be-3896-41d3-a8d1-97e1e33c623f", "startTime": 0, "content": "# This is a commentary about the video."}
+     */
 }
