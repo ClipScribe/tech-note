cdbc7618444f0644dd1cc015d14e3ed31e53fe97
kimzes <117077471+wonder002@users.noreply.github.com>
2024-11-13 01:45:20 +0900

    feat: Feedback Event Handler 구현

--- backend/llm-server/app/message_processor/message_processor.py
+++ backend/llm-server/app/message_processor/message_processor.py
@@ -3,6 +3,7 @@ import asyncio
 from loguru import logger
 
 from app.openai_service.ExplanationEventHandler import ExplanationEventHandler
+from app.openai_service.FeedbackEventHandler import FeedbackEventHandler
 from app.openai_service.IndexEventHandler import IndexEventHandler
 from app.openai_service.assistant_api_utils import *
 
@@ -60,37 +61,34 @@ async def create_chunk_explanation(chunk_file_path, assistant, request_id, chunk
     run = await run_stream(assistant.id, thread.id, event_handler=event_handler)
 
     # 진행 상황 로그
-    logger.info(f"설명문 생성 완료 ({chunk_index + 1}/{total_chunks})")
+    logger.info(f"{thread.id}-- 설명문 생성 완료 ({chunk_index + 1}/{total_chunks})")
     return thread, vector_store
 
 
 # 피드백 텍스트를 병렬로 생성하는 함수
-async def create_feedbacks_for_explanations(results, assistant):
+async def create_feedbacks_for_explanations(results, assistant, request_id):
     logger.info("피드백 텍스트 병렬 생성 시작")
 
     # 병렬 처리할 작업 목록
     tasks = []
 
     # 각 결과에서 thread와 vector_store를 사용하여 피드백 텍스트 생성 작업을 추가
-    for thread, vector_store in results:
-        task = create_chunk_feedback(thread, assistant)
+    for chunk_index,thread, vector_store in enumerate(results):
+        task = create_chunk_feedback(thread, assistant,vector_store,request_id=request_id,chunk_index = chunk_index)
         tasks.append(task)
 
     # 모든 피드백 생성 태스크를 병렬로 실행
     await asyncio.gather(*tasks)
 
-async def create_chunk_feedback(thread, assistant):
+async def create_chunk_feedback(thread, assistant, vector_store, request_id, total_chunks, chunk_index):
     logger.info(f"{thread.id}에 대한 피드백 텍스트 생성을 시작합니다.")
 
     # 피드백 이벤트 핸들러 생성
-    feedback_event_handler = FeedbackEventHandler(thread_id=thread.id, vector_store=vector_store)
+    feedback_event_handler = FeedbackEventHandler(thread_id=thread.id, vector_store=vector_store, request_id=request_id, chunk_index=chunk_index)
 
     # 피드백 텍스트 생성 시작
     await run_stream(assistant.id, thread.id, event_handler=feedback_event_handler)
-
-async def create_enhanced_explanation(thread, assistant):
-    logger.info("피드백 반영 최종 설명문 생성 시작")
-    run = await run_stream(assistant.id, thread.id, event_handler = ExplanationEventHandler())
+    logger.info(f"{thread.id}-- 설명문 피드백 생성 완료 ({chunk_index + 1})")
 
 # 설명문 생성 함수 (분할된 텍스트 파일들을 병렬 처리)
 async def create_enhanced_explanations(results, assistant):
